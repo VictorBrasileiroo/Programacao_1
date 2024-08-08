ecc752d6894ccdc261c12b35b9e6b454b9a5e9b4
Victor André Lopes Brasileiro <158325105+VictorBrasileiroo@users.noreply.github.com>
2024-08-08 20:36:03 -0300

    Update DueloNoRU.c

--- DueloNoRU.c
+++ DueloNoRU.c
@@ -1,129 +1,103 @@
 #include <stdio.h>
-#include <math.h>
 
-int rodadasBatalha(int classepedro, int classetulio, double vidapedro, double vidatulio, double ataquepedro, double ataquetulio){
+int batalha(int classePed, int classeTul, double *vidaPed, double *vidaTul, double *ataquePed, double *ataqueTul, int *contadorPedro, int *contadorTulio) {
+    int vencedorRodada;
 
-    int vencedorDaRodada; 
-    
-    if(classepedro == 1 && classetulio == 3){
-        vidatulio *= 1.3;
-    }else if(classepedro == 3 && classetulio == 1){
-        vidatulio *= 1.3;
-    //BONUS 2 - ARQUEIRO E MAGO
-    }else if(classepedro == 3 && classetulio == 2){
-        ataquepedro *= 1.25;
-    }else if(classepedro == 2 && classetulio == 3){
-        ataquetulio *= 1.25;
-    //BONUS 3 - MAGO E GUERREIRO
-    }else if(classepedro == 2 && classetulio == 1){
-        ataquepedro *= 1.15;
-        vidatulio *= 1.15;
-    }else if(classepedro == 1 && classetulio == 2){
-        ataquetulio *= 1.15;
-        vidatulio *= 1.15;
-    }else{
-        //NÃO FAZ NADA
+    if(classePed == 1 && classeTul == 3) {
+        *vidaPed *= 1.3;
+    } else if(classePed == 3 && classeTul == 1) {
+        *vidaTul *= 1.3;
+    } else if(classePed == 3 && classeTul == 2) {
+        *ataquePed *= 1.25;
+    } else if(classePed == 2 && classeTul == 3) {
+        *ataqueTul *= 1.25;
+    } else if(classePed == 2 && classeTul == 1) {
+        *ataquePed *= 1.15;
+        *vidaPed *= 1.15;
+    } else if(classePed == 1 && classeTul == 2) {
+        *ataqueTul *= 1.15;
+        *vidaTul *= 1.15;
     }
-    //ATACANDO QUEM TEM A MENOR VIDA
-    //PEDRO SENDO O QUE TEM MENOR VIDA E COMECANDO ATAACANDO
-    if(vidatulio < vidatulio || vidatulio == vidatulio){
-        //VIDA DO TULIO LEVANDO O ATAQUE  
-        vidatulio -= ataquepedro;
-        //VERIFICANDO SE A CARTA DO TULIO MORREU
-        if(vidatulio <= 0){
-            vencedorDaRodada = 1;
-        //SE A CARTA DE TULIO N MORREU AI A VAMOS PARA O ATAQUE DO TULIO 
-        }else{
-            vidatulio -= ataquetulio;
-            //VERIFICANDO A VIDA DO PEDRO
-            if(vidatulio <= 0){
-                vencedorDaRodada = 2;
-            //CASO NÃO TENHA MATADO A CARTA DO PEDRO VAMOS VERFICICAR QUEM TEM MAIS VIDA
-            }else{
-                if(vidatulio > vidatulio){
-                    vencedorDaRodada = 1;
-                }else if(vidatulio < vidatulio){
-                    vencedorDaRodada = 2;
 
-                //CASO ELES POSSUISEM A MESMA VIDA VAMOS VER QUEM TEM O MAIOR ATAQUE
-                }else{
-                    if(ataquepedro > ataquetulio){
-                        vencedorDaRodada = 1;
-
-                    }else{
-                        vencedorDaRodada = 2;
+    if(*vidaPed < *vidaTul || *vidaPed == *vidaTul) {
+        *vidaTul -= *ataquePed;
+        if(*vidaTul <= 0) {
+            vencedorRodada = 1;
+            (*contadorPedro)++;
+        } else {
+            *vidaPed -= *ataqueTul;
+            if(*vidaPed <= 0) {
+                vencedorRodada = 2;
+                (*contadorTulio)++;
+            } else {
+                if(*vidaPed > *vidaTul) {
+                    vencedorRodada = 1;
+                    (*contadorPedro)++;
+                } else if(*vidaPed < *vidaTul) {
+                    vencedorRodada = 2;
+                    (*contadorTulio)++;
+                } else {
+                    if(*ataquePed > *ataqueTul) {
+                        vencedorRodada = 1;
+                        (*contadorPedro)++;
+                    } else {
+                        vencedorRodada = 2;
+                        (*contadorTulio)++;
                     }
                 }
             }
         }
-    //SE O TULIO TIVER A MENOR VIDA E COMECANDO A RODADA
-    }else{
-        //VIDA O PEDRO LEVANDO DANO
-        vidatulio -= ataquetulio;
-        //VERIFICANDO SE A CARTA DO PEDRO MORREU
-        if(vidatulio <= 0){
-            vencedorDaRodada = 2;
-        }
-        //SE A CARTA DELE NÃO MORREU VAMOS PARA O ATAQUE DO PEDRRO
-        else{
-            vidatulio -= ataquepedro;
-            //VERIFICANDO A VIDA DO TULIO
-            if(vidatulio <= 0){
-                vencedorDaRodada = 1;
-            }
-            //CASO O TULIO N MORRA COM O ATAUQE DO PEDRO - VEREMOS QUEM TEM MAIS VIDA
-            else{
-                if(vidatulio > vidatulio){
-                    vencedorDaRodada = 1;
-                }else if(vidatulio < vidatulio){
-                    vencedorDaRodada = 2;
-
-                //CASO ELES POSSUISEM A MESMA VIDA VAMOS VER QUEM TEM O MAIOR ATAQUE
-                }else{
-                    if(ataquepedro > ataquetulio){
-                        vencedorDaRodada = 1;
-
-                    }else{
-                        vencedorDaRodada = 2;
-    
+    } else {
+        *vidaPed -= *ataqueTul;
+        if(*vidaPed <= 0) {
+            vencedorRodada = 2;
+            (*contadorTulio)++;
+        } else {
+            *vidaTul -= *ataquePed;
+            if(*vidaTul <= 0) {
+                vencedorRodada = 1;
+                (*contadorPedro)++;
+            } else {
+                if(*vidaPed > *vidaTul) {
+                    vencedorRodada = 1;
+                    (*contadorPedro)++;
+                } else if(*vidaPed < *vidaTul) {
+                    vencedorRodada = 2;
+                    (*contadorTulio)++;
+                } else {
+                    if(*ataquePed > *ataqueTul) {
+                        vencedorRodada = 1;
+                        (*contadorPedro)++;
+                    } else {
+                        vencedorRodada = 2;
+                        (*contadorTulio)++;
                     }
                 }
             }
         }
     }
-    return vencedorDaRodada;
+    return vencedorRodada;
 }
-
-int main(){
-
-    //INPUT
+int main() {
     int classePed1, classePed2, classePed3, classeTul1, classeTul2, classeTul3;
-    //CLASSES - 1 (Guerreiro), 2 (Mago) ou 3 (Arqueiro).
-    double vidaPed1, vidaPed2, vidaPed3, vidaTul1, vidaTul2, vidaTul3, ataquePed1, ataquePed2, ataquePed3, ataqueTul1, ataqueTul2, ataqueTul3; 
+    double vidaPed1, vidaPed2, vidaPed3, vidaTul1, vidaTul2, vidaTul3;
+    double ataquePed1, ataquePed2, ataquePed3, ataqueTul1, ataqueTul2, ataqueTul3;
+    scanf("%d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf",
+          &classePed1, &ataquePed1, &vidaPed1, &classePed2, &ataquePed2, &vidaPed2,
+          &classePed3, &ataquePed3, &vidaPed3, &classeTul1, &ataqueTul1, &vidaTul1,
+          &classeTul2, &ataqueTul2, &vidaTul2, &classeTul3, &ataqueTul3, &vidaTul3);
 
-    scanf("%d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf %d%lf%lf", &classePed1,&ataquePed1,&vidaPed1, &classePed2,&ataquePed2,&vidaPed2, &classePed3,&ataquePed3,&vidaPed3, &classeTul1,&ataqueTul1,&vidaTul1, &classeTul2,&ataqueTul2,&vidaTul2, &classeTul3,&ataqueTul3,&vidaTul3);
+        int contadorPedro = 0;
+        int contadorTulio = 0;
+        int resultadoRodada1 = batalha(classePed1, classeTul1, &vidaPed1, &vidaTul1, &ataquePed1, &ataqueTul1, &contadorPedro, &contadorTulio);
+        int resultadoRodada2 = batalha(classePed2, classeTul2, &vidaPed2, &vidaTul2, &ataquePed2, &ataqueTul2, &contadorPedro, &contadorTulio);
+        int resultadoRodada3 = batalha(classePed3, classeTul3, &vidaPed3, &vidaTul3, &ataquePed3, &ataqueTul3, &contadorPedro, &contadorTulio);
 
-    int rodada1 = rodadasBatalha(classePed1,classeTul1,vidaPed1,vidaTul1,ataquePed1,ataqueTul1);
-    int rodada2 = rodadasBatalha(classePed2,classeTul2,vidaPed2,vidaTul2,ataquePed2,ataqueTul2);
-    int rodada3 = rodadasBatalha(classePed3,classeTul3,vidaPed3,vidaTul3,ataquePed3,ataqueTul3);
-
-    if(rodada1 == 1){
-        printf("Rodada1: Pedro\n");
-    }else{
-        printf("Rodada1: Tulio\n");
-    }
-
-    if(rodada2 == 1){
-        printf("Rodada2: Pedro\n");
-    }else{
-        printf("Rodada2: Tulio\n");
-    }
-
-    if(rodada3 == 1){
-        printf("Rodada3: Pedro\n");
-    }else{
-        printf("Rodada3: Tulio\n");
-    }
+            printf("Rodada1: %s\n", resultadoRodada1 == 1 ? "Pedro" : "Tulio");
+            printf("Rodada2: %s\n", resultadoRodada2 == 1 ? "Pedro" : "Tulio");
+            printf("Rodada3: %s\n", resultadoRodada3 == 1 ? "Pedro" : "Tulio");
+            printf("%s vitorioso", contadorPedro > contadorTulio ? "Pedro" : "Tulio");
 
     return 0;
 }
+
