bd34f8cea750652d5939e0c6011751ca9ff9967f
Victor André Lopes Brasileiro <158325105+VictorBrasileiroo@users.noreply.github.com>
2024-08-20 11:20:22 -0300

    Update Soma de Algarismos.c

--- Soma de Algarismos.c
+++ Soma de Algarismos.c
@@ -3,6 +3,14 @@ int somarDigitos(int numero) {
     if (numero == 0) {
         return 0;
     }
+    //aqui ele soma o ultimo numero com a recursão dos outros 3 numeoros
+        // ex: 1234 - 4 + somarDigitos(123)
+        // 3 + somarDigitos(12)
+        // 2 + somarDigitos(1)
+        // somarDigitos(1) = 1 + 0
+        //2 + 1 = 3
+        //3 + 3 = 6
+        //4 + 6 = 10
     return (numero % 10) + somarDigitos(numero / 10);
 }
 int main(){
